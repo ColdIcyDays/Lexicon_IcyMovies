b0d0daa146a10fb34f9e419d12466785eb1d8d54
ColdIcyDays <erik.ljungman@gmail.com>
2026-06-01 16:11:27 +0200

    Added database migrations

---
+++ IcyMovies/Models/Review.cs
@@ -0,0 +1,10 @@
+﻿namespace IcyMovies.Models;
+
+public class Review
+{
+    public int Id { get; set; }
+    public int MovieId { get; set; }
+    public Movie TargetMovie { get; set; }
+    public string ReviewTitle { get; set; }
+    public string ReviewBody { get; set; }
+}
